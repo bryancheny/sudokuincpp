42c132ccddaab7e3921e748de0181379194684ca
Bryan Chen <bryanchenyu616@gmail.com>
2022-08-25 11:44:34 +0800

    Cross Platform Utility

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream> 
+#include "output.h"
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "hello!!";
+    std::cout << "This text will be erased.\n";
+    cls();
+    #ifdef _WIN32
+        std::cout << "hello Windows!!";
+    #else 
+        std::cout << "Hello Apple/Linux!";
+    #endif
     return 0;
 }
