44cdc814e9417c758b8d6ea96b2dfa2c8105cf1a
Bryan Chen <bryanchenyu616@gmail.com>
2022-08-26 10:55:08 +0800

    Update Readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # terminaltyping
 
-Command line typing game! Check your WPM and CPM typing scores in a flash!
+Cross platform command line typing game! Check your WPM and CPM typing scores in a flash! Work in progress
+
+## How to run
+
+#### Linux/MacOS
+```
+sh build.sh
+./bin/terminaltyping
+```
+
+#### Windows
+```
+Run build.bat 
+Run executable in bin folder
+```
+
+
--- src/output.h
+++ src/output.h
@@ -4,6 +4,15 @@
 
 #ifdef _WIN32
 #include <conio.h>
+#include <windows.h> 
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+// you can loop k higher to see more color choices
+for(int k = 1; k < 255; k++)
+{
+// pick the colorattribute k you want
+SetConsoleTextAttribute(hConsole, k);
+cout << k << " I want to be nice today!" << endl;
+}
 #else
     #ifdef __linux__
     #include <termio.h>
@@ -43,5 +52,6 @@ inline void cls(void)
     system("clear");
 #endif
 }
+
 
 #endif
